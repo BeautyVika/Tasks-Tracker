56353d49e05299ed2236ca9de64c169101fb27f3
BeautyVika <vikachk54@gmail.com>
2023-05-17 17:37:08 +0300

    added routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,5 @@
 import React, {useEffect} from 'react'
 import Container from '@mui/material/Container'
-import TodolistsList from "../features/todolistsList/TodolistsList"
-import {Navigate, Route, Routes} from "react-router-dom"
-import {Login} from "features/auth/Login"
 import {authThunks} from "features/auth/auth.reducer"
 import {AppUseSelector} from "./store"
 import {CircularProgress} from "@mui/material"
@@ -10,11 +7,11 @@ import {selectIsInitialized} from "app/app.selectors"
 import {ButtonAppBar, ErrorSnackbar} from "common/components"
 import './App.css'
 import {useActions} from "common/hooks/useAction"
+import {AppRoutes} from "app/appRoutes/AppRoutes"
 
 function App() {
 
     const isInitialized = AppUseSelector(selectIsInitialized)
-
     const {me} = useActions(authThunks)
 
     useEffect(() => {
@@ -33,14 +30,7 @@ function App() {
             <ErrorSnackbar/>
             <ButtonAppBar/>
             <Container fixed>
-                <Routes>
-                    <Route path={'/'} element={<TodolistsList/>}/>
-                    <Route path={'/auth'} element={<Login/>}/>
-                    <Route path={'/404'}
-                           element={<h1 style={{display: 'flex',justifyContent:'center', alignItems: 'center'}}>404 not found</h1>}/>
-                    <Route path={'*'}
-                           element={<Navigate to={'/404'}/>}/>
-                </Routes>
+                <AppRoutes/>
             </Container>
         </div>
     );
